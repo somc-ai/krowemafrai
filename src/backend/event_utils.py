803011af410dabc43bd77bc0334e8add0288deee
somc-ai <info@so-mc.nl>
2025-07-15 11:07:19 +0200

    Update event_utils.py

--- src/backend/event_utils.py
+++ src/backend/event_utils.py
@@ -1,6 +1,6 @@
 import logging
 import os
-# from azure.monitor.events.extension import track_event
+from azure.monitor.events.extension import track_event
 
 
 def track_event_if_configured(event_name: str, event_data: dict):
